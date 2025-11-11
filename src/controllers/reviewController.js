7e332e2c96c150c6c80d0ab2a978b71e20ee8cb6
Abdul Azeem <abdulazeem@devnexus.in>
2025-11-11 15:44:17 +0530

    changes in quiz and reviews

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -224,3 +224,83 @@ export const deleteReview = async (req, res) => {
     });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'User not found' });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(401).json({ msg: 'Invalid credentials' });
+
+    //  Generate Access & Refresh tokens
+    const { accessToken, refreshToken, expiresIn } = generateToken(user);
+
+    //  Save refresh token in DB
+    user.refreshToken = refreshToken;
+    user.lastLogin = new Date();
+    user.loginHistory.push({
+      loginAt: user.lastLogin,
+      ip: req.ip,
+      userAgent: req.headers['user-agent'],
+    });
+    await user.save();
+
+    // Store refresh token in secure HTTP-only cookie
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/api/refresh',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+
+    //  Fetch user quiz history (optional)
+    const quizAttempts = await QuizAttemptModel.find({ userId: user._id }).lean();
+    // console.log('quizAttempts in login', quizAttempts);
+
+    const detailedAttempts = await Promise.all(
+      quizAttempts.map(async (attempt) => {
+        const quiz = await quizModel.findOne({ id: attempt.quizId }).lean();
+        // console.log('quiz in login', quiz);
+
+        if (!quiz) return attempt;
+
+        return {
+          quizId: attempt.quizId,
+          quizTitle: quiz.title,
+          score: attempt.score,
+          totalMarks: attempt.totalMarks,
+          attemptedAt: attempt.attemptedAt,
+          answers: attempt.answers.map((ans) => {
+            const q = quiz.questions.find((qq) => qq.id === ans.questionId);
+            return {
+              question: q ? q.question : 'Question not found',
+              options: q ? q.options : [],
+              selectedIndex: ans.selectedIndex,
+              correctAnswerIndex: ans.correctAnswerIndex,
+              isCorrect: ans.isCorrect,
+            };
+          }),
+        };
+      }),
+    );
+
+    const userData = user.toObject();
+    delete userData.password;
+
+    res.status(200).json({
+      message: 'Login successful',
+      accessToken,
+      expiresIn,
+      user: {
+        ...userData,
+        attemptedQuizzes: detailedAttempts,
+      },
+    });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ msg: 'Server error during login', error: error.message });
+  }
+};
