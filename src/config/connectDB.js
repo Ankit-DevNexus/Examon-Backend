3ba6a2e753a66ad93caf721bbe0fc50c7dbb5acb
Abdul Azeem <abdulazeem@devnexus.in>
2025-11-12 13:58:54 +0530

    chnge in signup

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 export const ConnectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URI);
+
     console.log('Connected to mongo DB');
   } catch (error) {
     console.error('Dashboard DB connection error:', error.message);
