511cac67436dbead4fd8f5b7bff40520989191c9
Abdul Azeem <abdulazeem@devnexus.in>
2025-11-24 10:55:39 +0530

    changes in notification

--- src/models/notificationModel.js
+++ src/models/notificationModel.js
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
 
-const notificationSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  subtitle: { type: String },
-  description: { type: String },
-  link: { type: String },
-  image: { type: String },
-  publicId: { type: String },
-  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
-  createdAt: { type: Date, default: Date.now },
-});
+const notificationSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    subtitle: { type: String },
+    description: { type: String },
+    link: { type: String },
+    image: { type: String },
+    publicId: { type: String },
+    status: { type: String, enum: ['active', 'inactive'], default: 'active' },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const notificationModel = mongoose.model('Notification', notificationSchema);
 export default notificationModel;
