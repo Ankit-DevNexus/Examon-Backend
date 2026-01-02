435351223ee791e7fdeeb0423f9fbdb216c2ec01
abdulDevnexus9634 <abdulazeem@devnexus.in>
2026-01-02 14:12:29 +0530

    change in ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,15 @@ module.exports = {
   apps: [
     {
       name: "examon-backend",
-      script: "src/index.js", // adjust if entry file differs
+      script: "src/index.js",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "400M",
+
       env: {
         NODE_ENV: "production",
+        DOTENV_CONFIG_PATH: "./src/.env",
       },
     },
   ],
